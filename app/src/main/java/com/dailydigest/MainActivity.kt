a3b2de4a56be87c5fa84d4ba491784023fc60ac7
Dhairya Jain <dhairyajainrocks@gmail.com>
2023-07-07 08:31:38 +0530

    Added activity binding -d 07-07-2023

--- app/src/main/java/com/dailydigest/MainActivity.kt
+++ app/src/main/java/com/dailydigest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.dailydigest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dailydigest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
